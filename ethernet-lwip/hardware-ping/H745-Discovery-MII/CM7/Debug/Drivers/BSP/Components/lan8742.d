72dbf168a3d9994ab976f2a24ea20a3ce035c266
Arun Rawat <arunrawat@ArunsMacmini.local>
2026-02-27 16:28:44 +0530

    Ethernet UDP Server: Added full project for H745 Discovery

--- ethernet-lwip/hardware-ping/H745-Discovery-MII/CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- /Users/arunrawat/Desktop/H745-Discovery-MII/Drivers/BSP/Components/lan8742/lan8742.c \
- /Users/arunrawat/Desktop/H745-Discovery-MII/Drivers/BSP/Components/lan8742/lan8742.h
-/Users/arunrawat/Desktop/H745-Discovery-MII/Drivers/BSP/Components/lan8742/lan8742.h:
