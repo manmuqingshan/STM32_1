5d4c1590a3b50edd72632e081158bbb7295a3421
Arun Rawat <arunrawat@ArunsMacmini.local>
2026-08-06 13:54:14 +0530

    added project files

---
+++ custom-bootloader/application_validation/F103/F103_Bootloader/Core/Src/bl_jump.c
@@ -0,0 +1,63 @@
+/*
+ * bl_jump.c
+ *
+ *  Created on: 16-Dec-2025
+ *      Author: arunrawat
+ */
+
+
+#include "main.h"
+#include "bl_jump.h"
+#include "flash_layout.h"
+#include "app_header.h"
+
+
+#define APP_MAGIC 	0xABCDEFAB
+
+typedef void (*pFunction)(void);
+
+void JumpToApplication(void)
+{
+    uint32_t appStack;
+    uint32_t appResetHandler;
+    pFunction appEntry;
+
+    /* Read application stack pointer */
+    appStack = *(volatile uint32_t*)APP_START_ADDR;
+
+
+    /* Read reset handler address */
+    appResetHandler = *(volatile uint32_t*)(APP_START_ADDR + 4);
+    appEntry = (pFunction)appResetHandler;
+
+    /* Disable interrupts */
+    __disable_irq();
+
+    /* Stop SysTick */
+    SysTick->CTRL = 0;
+    SysTick->LOAD = 0;
+    SysTick->VAL  = 0;
+
+    /* Set main stack pointer */
+    __set_MSP(appStack);
+
+    /* Jump to application reset handler */
+    appEntry();
+}
+
+int bootloader_is_app_valid(void)
+{
+	uint32_t HDR_ADDR = APP_HEADER_ADDR;
+    const app_header_t *app_hdr = (const app_header_t *)HDR_ADDR;
+
+    /* 1. Magic */
+    if (app_hdr->magic != APP_MAGIC)
+        return 1;
+
+    /* 2. Reset handler sanity */
+    uint32_t reset_handler = *(uint32_t *)(APP_START_ADDR + 4);
+    if ((reset_handler & 0xFF000000) != 0x08000000)
+        return 2;
+
+    return 0;   // VALID
+}
